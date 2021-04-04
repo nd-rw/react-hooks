b80867e8a2e98cafbc673d916fa521024f74d432
andrewlarder <andrew.larder@glxdigital.com>
2021-04-04 23:19:22 +0800

    ex4 up to extra credit 1

--- src/exercise/04.js
+++ src/exercise/04.js
@@ -3,9 +3,39 @@
 
 import * as React from 'react'
 
+import {useLocalStorageState} from '../utils';
+
+function useStickyState(defaultValue, key) {
+  const [value, setValue] = React.useState(() => {
+    const stickyValue = window.localStorage.getItem(key);
+    return stickyValue !== null
+      ? JSON.parse(stickyValue)
+      : defaultValue;
+  });
+  React.useEffect(() => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }, [key, value]);
+  return [value, setValue];
+}
+
 function Board() {
   // 🐨 squares is the state for this component. Add useState for squares
-  const squares = Array(9).fill(null)
+  const localStorageValue = window.localStorage.getItem('squares');
+  console.log("🚀 ~ file: 04.js ~ line 22 ~ Board ~ localStorageValue", localStorageValue)
+
+  // check localStorage, if empty then set to array filled with null values
+  const [squares, setSquares] = React.useState(() => {
+    return localStorageValue !== null ? JSON.parse(localStorageValue) : Array(9).fill(null)
+  });
+
+  // when "squares changes" update localStorage
+  React.useEffect(() => {
+    window.localStorage.setItem('squares', JSON.stringify(squares))
+  }, [squares])
+
+  const nextValue = calculateNextValue(squares);
+  const winner = calculateWinner(squares);
+  const status = calculateStatus(winner, squares, nextValue);
 
   // 🐨 We'll need the following bits of derived state:
   // - nextValue ('X' or 'O')
@@ -17,25 +47,34 @@ function Board() {
   // This is the function your square click handler will call. `square` should
   // be an index. So if they click the center square, this will be `4`.
   function selectSquare(square) {
+
     // 🐨 first, if there's already winner or there's already a value at the
     // given square index (like someone clicked a square that's already been
     // clicked), then return early so we don't make any state changes
-    //
+    if (winner !== null) {
+      console.log(winner + " is winner so function doesn't go further");
+      return null;
+    }
     // 🦉 It's typically a bad idea to mutate or directly change state in React.
     // Doing so can lead to subtle bugs that can easily slip into production.
     //
     // 🐨 make a copy of the squares array
     // 💰 `[...squares]` will do it!)
-    //
+
+    let squaresCopy = [...squares];
+
     // 🐨 set the value of the square that was selected
     // 💰 `squaresCopy[square] = nextValue`
-    //
+    squaresCopy[square] = nextValue;
+
     // 🐨 set the squares to your copy
+    setSquares(squaresCopy);
   }
 
   function restart() {
     // 🐨 reset the squares
     // 💰 `Array(9).fill(null)` will do it!
+    setSquares(Array(9).fill(null));
   }
 
   function renderSquare(i) {
@@ -49,7 +88,7 @@ function Board() {
   return (
     <div>
       {/* 🐨 put the status in the div below */}
-      <div className="status">STATUS</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -93,13 +132,24 @@ function calculateStatus(winner, squares, nextValue) {
 
 // eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
+  // how many "X" values are on the board?
   const xSquaresCount = squares.filter(r => r === 'X').length
+
+  // how many "O" values are on the board?
   const oSquaresCount = squares.filter(r => r === 'O').length
+
+  // if there are the same amount of "X" and "O" values, "X" is the next value/turn
+  // otherwise the next turn/value is "O"
+  // seems like a somewhat hack-y solution to odd/even but is probably solid
+
   return oSquaresCount === xSquaresCount ? 'X' : 'O'
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
+
+  // a list of possible lines through the board
+  // first few are simple horozontal lines, rest continue as you'd expect
   const lines = [
     [0, 1, 2],
     [3, 4, 5],
