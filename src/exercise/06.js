85b6a8ba431d5cce245f4904e434b7a08a2e3733
andrewlarder <andrew.larder@glxdigital.com>
2021-04-07 00:10:28 +0800

    exercise 06, extra credit 02 and 03

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,27 +6,72 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import { PokemonForm, fetchPokemon, PokemonDataView, PokemonInfoFallback } from '../pokemon'
 
-function PokemonInfo({pokemonName}) {
+function PokemonInfo({ pokemonName }) {
+
+
   // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => { /* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+  const [state, setState] = React.useState({
+    status: 'idle',
+    pokemon: null,
+    error: null,
+  })
+  const { status, pokemon, error } = state
 
-  // 💣 remove this
-  return 'TODO'
+
+  React.useEffect(() => {
+    if (pokemonName === '') {
+      return
+    } else {
+      // this resets the result to show the loading/no pokemon value after consecutive searches
+      setState({
+        status: 'idle',
+        pokemon: null,
+        error: null,
+      });
+
+      fetchPokemon(pokemonName).then(pokemonData => {
+        setState(
+          {
+            status: 'resolved',
+            pokemon: pokemonData,
+            error: null
+          }
+        )
+      }).catch(error => setState({
+        status: 'rejected',
+        pokemon: null,
+        error: error,
+      }))
+    }
+  }, [pokemonName])
+
+  const ErrorMesssage = ({ error }) => {
+    return <div role="alert">
+      There was an error: <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
+      <pre style={{ whiteSpace: 'normal' }}>Status: {status}</pre>
+    </div>
+  }
+
+  const Info = () => {
+    if (error) {
+      return <ErrorMesssage error={error} />
+    }
+    if (pokemonName !== '' && pokemon !== null) {
+      return <PokemonDataView pokemon={pokemon} />
+    }
+    else if (pokemonName !== '' && pokemon === null) {
+      return <PokemonInfoFallback name={pokemonName} />
+    }
+    else if (pokemonName === '' && pokemon === null) {
+      return <>Submit a pokemon</>
+    }
+  }
+
+  return <Info />
 }
+
 
 function App() {
   const [pokemonName, setPokemonName] = React.useState('')
